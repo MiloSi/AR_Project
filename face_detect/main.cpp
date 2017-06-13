82c651a465e6d7c79a3c817701e11e7cf82854e3
C <aquarius456@nate.com>
2017-06-13 23:16:28 +0900

    Add files via upload

---
+++ face_detect/main.cpp
@@ -0,0 +1,63 @@
+
+#pragma warning (disable : 4996)
+#include <iostream>
+#include <opencv2\highgui.hpp>
+#include <opencv2\imgproc.hpp>
+#include <opencv2\objdetect.hpp>
+#include <opencv2\core\ocl.hpp>
+
+#include <vector>
+
+
+using namespace std;
+using namespace cv;
+using namespace cv::ocl;
+
+
+int main()
+{
+	VideoCapture cap(0);
+	CascadeClassifier fd("haarcascade_frontalface_default.xml");
+
+	Mat frame; 
+	UMat gray;
+
+	//vector faces;
+
+	for (;;)
+	{
+		cap >> frame;
+
+		int width = frame.cols;
+		int height = frame.rows;
+
+		cvtColor(frame, gray, COLOR_BGR2GRAY);
+		equalizeHist(gray, gray);
+
+		resize(gray, gray, Size(width * 0.5, height * 0.5));
+
+		vector<Rect> rects;
+		fd.detectMultiScale(gray, rects, 1.1, 2, 0);
+
+		for (Rect object : rects) {
+			Point center(object.x * + object.width / 2, object.y + object.height / 2);
+			object.x *= 2;
+			object.y *= 2;
+			object.width *= 2;
+			object.height *= 2;
+
+			rectangle(frame, object, Scalar(255, 0, 255), 3);
+		}
+
+		imshow("hello", frame);
+
+		if (waitKey(40) >= 0) {
+			imwrite("captured.jpg", frame);
+			break;
+		}
+	}
+
+
+	return 0;
+
+}
